fbfa2a55fdd8b33493f5247b6ae605d0f663f697
arifraj199 <karif9514@gmail.com>
2024-04-23 12:32:43 +0600

    update user functionality implement

--- src/components/UpdateUser/UpdateUser.jsx
+++ src/components/UpdateUser/UpdateUser.jsx
@@ -1,7 +1,38 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const UpdateUser = () => {
   const loadUser = useLoaderData();
+  const navigate = useNavigate();
+  const handleUpdateForm = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const gender = form.gender.value;
+    const status = form.status.value;
+    const updateUser = { name, email, gender, status };
+    console.log(updateUser);
+    fetch("http://localhost:5000/users", {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updateUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          Swal.fire({
+            title: "Updated Successfully!",
+            text: "See your updated info in all user",
+            icon: "success",
+          });
+          navigate("/users");
+        }
+      });
+  };
   return (
     <div className="w-3/4 mx-auto flex justify-center my-20">
       <div className=" w-1/2">
@@ -12,7 +43,10 @@ const UpdateUser = () => {
           </p>
         </div>
         <hr className="my-6 w-full" />
-        <form className="flex flex-col items-center w-full">
+        <form
+          onSubmit={handleUpdateForm}
+          className="flex flex-col items-center w-full"
+        >
           <h2 className="text-xl mb-8 mt-4">Update Info</h2>
           <div className="flex flex-col mb-6 gap-2 w-3/4">
             <label>Name</label>
